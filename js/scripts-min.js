b36c890a9df71d85a62780fb909593b5dcbe6098
Madeline <mbrine@vcd-ceb131-01.eastern.ewu.edu>
2017-04-13 15:55:28 -0700

    working weather

---
+++ js/scripts-min.js
@@ -0,0 +1,1 @@
+$.simpleWeather({location:99004,unit:"f",success:function(t){console.log(t),$(".temp").text(t.temp),$(".city").text(t.city),$(".image img").attr("src",t.image),$(".description").text(t.text)},error:function(t){console.log("Look outside.")}});
--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,24 @@
-// Scripts
-console.log('Hello World!');
+// Scriptz
+
+
+$.simpleWeather({
+    location: 99004,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+            $('.temp').text(weather.temp);
+        $('.city').text(weather.city);
+        $('.image img').attr('src', weather.image);
+        $('.description').text(weather.text);
+
+      
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
